92f0c348b110e87327cbb665163f850337382420
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-10-05 10:43:03 -0700

    update to latest version of Vite + SvelteKit

---
+++ svelte-kit-app/vite.config.js
@@ -0,0 +1,11 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	ssr: {
+		external: ['tmp-context-issue']
+	}
+};
+
+export default config;
